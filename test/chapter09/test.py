e9e7c0731043c7b2da2ea2868b17376c96719587
mensylisir <710080675@qq.com>
2019-10-31 15:47:07 +0800

    代理的简单使用

---
+++ test/chapter09/test.py
@@ -0,0 +1,27 @@
+import requests
+from urllib.error import URLError
+from urllib.request import ProxyHandler, build_opener
+
+proxy = '127.0.0.1:7742'
+proxy_handler = ProxyHandler({
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy
+})
+opener = build_opener(proxy_handler)
+try:
+    response = opener.open('http://httpbin.org/get')
+    print(response.read().decode('utf-8'))
+except URLError as e:
+    print(e.reason)
+
+
+proxy = '127.0.0.1:7742'
+proxies = {
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy
+}
+try:
+    response = requests.get('http://httpbin.org/get', proxies=proxies)
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print(e.args)
