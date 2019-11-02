bfd43c08f458088466323af4be541d366e6c5fe4
mensylisir <710080675@qq.com>
2019-11-02 20:59:47 +0800

    requests库设置代理

--- test/chapter09/test1.py
+++ test/chapter09/test1.py
@@ -1,3 +1,7 @@
+import requests
+from urllib import request
+import socks
+import socket
 from urllib.error import URLError
 from urllib.request import ProxyHandler, build_opener
 
@@ -12,4 +16,26 @@ try:
     response = opener.open('http://httpbin.org/get')
     print(response.read().decode('utf-8'))
 except URLError as e:
+    print(e)
+
+
+socks.setdefaultproxy(socks.SOCKS5, '127.0.0.1', '7453')
+socket.socket = socks.socksocket
+try:
+
+    response = request.urlopen('http://httpbin.org/get')
+    print(response.read().decode('utf-8'))
+except URLError as e:
+    print(e)
+
+
+proxy = '127.0.0.1:7742'
+proxies = {
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy
+}
+try:
+    response = requests.get('http://httpbin.org/get', proxies=proxies)
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
     print(e)
